5e8a671a5c2d7f13a16a52c313b39f8cbe85645f
unknown <r.rafi1615005@gmail.com>
2023-11-17 12:37:04 +0600

    add login-endpoint

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,19 +1,51 @@
 const router = require("express").Router();
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
-
+// register
 router.post("/register", async (req, res) => {  
     const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET),
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString(),
         created_by: req.body.created_by,
     })
 
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser)     
+    } catch (error) {
+        res.status(500).json(error) 
+    }
+})
+
+// login
+router.post("/login", async (req, res) => {  
+    console.log(req.body)
+    try {
+        const user = await User.findOne({name: req.body.name})
+
+        if (!user) {
+            res.status(401).json("No user with that name!") 
+        } else {  
+            const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET)
+            const DBpassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+    
+            if (req.body.password !== DBpassword) {
+                res.status(401).json("wrong credentials!")
+            } else {
+                const accessToken = jwt.sign({
+                    id:user._id,
+                    created_by: user.created_by
+                }, process.env.JWT_SECRET,
+                {expiresIn:"4h"}
+                )
+                const {password, ...otherInfo} = user._doc
+                res.status(200).json({...otherInfo, accessToken})     
+            }         
+        }
+
     } catch (error) {
         res.status(500).json(error) 
     }
