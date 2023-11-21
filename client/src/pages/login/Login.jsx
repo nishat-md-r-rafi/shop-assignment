7d3aba56bca6c6ad08dbf15f82b9ce9991187f1b
unknown <r.rafi1615005@gmail.com>
2023-11-21 15:22:13 +0600

    validate-auth-form

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -10,26 +10,49 @@ export const Login = () => {
     const handleSignUp = (e) => {  
         e.preventDefault()
     }
-    
+
   return (
     <div className='login'>
         <div className={isActive?'container active': 'container'} id='container'>
+
+            // sign-up
             <div className="form-container sign-up">
                 <form onSubmit={handleSignUp}>
                     <h1>Create Account</h1>
-                    <input type="text" placeholder="Name"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+                    <input 
+                        type="text" 
+                        placeholder="Name between 3-16 without special character!"
+                        name="name"
+                        pattern= "^[A-Za-z0-9]{3,16}$"
+                        required
+                        />
+                    <input type="email" placeholder="Email" required/>
+                    <input 
+                        type="password" 
+                        placeholder="Password between 4-20, 1 letter, 1 number, 1 special character!" 
+                        pattern= "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,20}$"
+                        required/>
                     <button>Sign Up</button>
                 </form>
             </div>
 
+            // sign-in
             <div className="form-container sign-in">
                 <form onSubmit={handleSignIn}>
                     <h1>Sign In</h1>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <a >Forget Your Password?</a>
+                    <input 
+                        type="text" 
+                        placeholder="Name between 3-16 without special character!"
+                        name="name"
+                        pattern= "^[A-Za-z0-9]{3,16}$"
+                        required
+                        />
+                    <input 
+                        type="password" 
+                        placeholder="Password between 4-20,1 letter, 1 number,1 special character!" 
+                        pattern= "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,20}$"
+                        required
+                    />
                     <button>Sign In</button>
                 </form>
             </div>
@@ -48,6 +71,7 @@ export const Login = () => {
                 </div>
             </div>
         </div>
+
         </div>
     </div>
   )
